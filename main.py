823323ad0510f3133bbc61f0a0bee5e464db57d7
Arnav Chilakala <aschilak@calpooly.edu>
2026-05-20 11:23:27 -0700

    data definitions

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import string
 
 sys.setrecursionlimit(10**6)
 
+IntList : TypeAlias = Optional['LLNode'] 
+@dataclass(frozen=True)
+class LLNode:
+  value : int
+  rest : IntList
+
+class WordLines:
+  key : str
+  lines : IntList
+
+WordLinesList : TypeAlias = Optional['WLNode']
+@dataclass(frozen=True)
+class WLNode:
+  value : WordLines
+  rest : WordLinesList
+
+class HashTable:
+  bins : List[WordLinesList]
+  count : int
 
 class Tests(unittest.TestCase):
   pass
